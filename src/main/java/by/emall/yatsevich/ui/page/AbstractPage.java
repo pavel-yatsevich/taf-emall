992d0cc91ca20d8e3a3b7075302a61f8a66271ab
lu55y <mu11er100500@gmail.com>
2024-09-16 20:49:42 +0300

    add AbstractPage class

---
+++ src/main/java/by/emall/yatsevich/ui/page/AbstractPage.java
@@ -0,0 +1,14 @@
+package by.emall.yatsevich.ui.page;
+
+import org.openqa.selenium.WebDriver;
+
+public abstract class AbstractPage {
+
+    protected WebDriver driver;
+
+    public AbstractPage(WebDriver driver){
+        this.driver = driver;
+    }
+
+    public abstract AbstractPage openPage();
+}
