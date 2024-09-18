952942d35faed58a861514e76b65d330ded3b99d
lu55y <mu11er100500@gmail.com>
2024-09-18 19:44:00 +0300

    add EmallMainPage class

---
+++ src/main/java/by/emall/yatsevich/ui/page/EmallMainPage.java
@@ -0,0 +1,14 @@
+package by.emall.yatsevich.ui.page;
+
+import org.openqa.selenium.WebDriver;
+
+public class EmallMainPage extends AbstractPage {
+    public EmallMainPage(WebDriver driver) {
+        super(driver);
+    }
+
+    @Override
+    public EmallMainPage openPage() {
+        return null;
+    }
+}
