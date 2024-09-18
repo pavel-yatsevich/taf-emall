4ac4ba80e101f3afe38e44d494225be7ef894dbe
lu55y <mu11er100500@gmail.com>
2024-09-18 20:47:17 +0300

    REFACTOR EmallMainPage class

--- src/main/java/by/emall/yatsevich/ui/page/EmallMainPage.java
+++ src/main/java/by/emall/yatsevich/ui/page/EmallMainPage.java
@@ -3,12 +3,17 @@ package by.emall.yatsevich.ui.page;
 import org.openqa.selenium.WebDriver;
 
 public class EmallMainPage extends AbstractPage {
+
+    public static final String EMALL_MAIN_PAGE_LOGIN_FORM_BUTTON_XPATH = "//button[@class='touchable_button__GJNQZ actions_action__button__OiPrx']";
+    private static final String BASE_URL = "https://emall.by/";
+
     public EmallMainPage(WebDriver driver) {
         super(driver);
     }
 
     @Override
     public EmallMainPage openPage() {
-        return null;
+        driver.navigate().to(BASE_URL);
+        return this;
     }
 }
