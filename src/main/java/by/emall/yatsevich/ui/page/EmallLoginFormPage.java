752dd34266e833a138dc889d32ca38c1ad1a726a
lu55y <mu11er100500@gmail.com>
2024-09-19 22:12:35 +0300

    ADD EmallLoginFormPageConstants interface

--- src/main/java/by/emall/yatsevich/ui/page/EmallLoginFormPage.java
+++ src/main/java/by/emall/yatsevich/ui/page/EmallLoginFormPage.java
@@ -1,12 +1,10 @@
 package by.emall.yatsevich.ui.page;
 
+import by.emall.yatsevich.ui.page.constants.EmallLoginFormPageConstants;
 import by.emall.yatsevich.ui.utils.Waiters;
 import org.openqa.selenium.By;
 
-public class EmallLoginFormPage extends AbstractPage {
-
-    private static final String LOGIN_PAGE_URL = "https://emall.by/login";
-    public static final String LOGIN_PAGE_BUTTON_ENTER_BY_PASSWORD_XPATH = "//button[@class='btn_btn__QQYBS btn_btn_colour_black__V_2g5 btn_btn_size_medium__oY0HW btn_btn_type_secondary___G_V0 login_button__zjbHA login_enter_button__WJQjN']";
+public class EmallLoginFormPage extends AbstractPage implements EmallLoginFormPageConstants {
 
     private final By LOGIN_PAGE_BUTTON_ENTER_BY_PASSWORD_BY = By.xpath(LOGIN_PAGE_BUTTON_ENTER_BY_PASSWORD_XPATH);
 
---
+++ src/main/java/by/emall/yatsevich/ui/page/constants/EmallLoginFormPageConstants.java
@@ -0,0 +1,7 @@
+package by.emall.yatsevich.ui.page.constants;
+
+public interface EmallLoginFormPageConstants {
+
+    String LOGIN_PAGE_URL = "https://emall.by/login";
+    String LOGIN_PAGE_BUTTON_ENTER_BY_PASSWORD_XPATH = "//button[@class='btn_btn__QQYBS btn_btn_colour_black__V_2g5 btn_btn_size_medium__oY0HW btn_btn_type_secondary___G_V0 login_button__zjbHA login_enter_button__WJQjN']";
+}
