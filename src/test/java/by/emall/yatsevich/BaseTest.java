3e77dd7c4356124ebe2c49c3ae91eabf3a261071
lu55y <mu11er100500@gmail.com>
2024-09-23 21:27:16 +0300

    REFACTOR BaseTest class

--- src/test/java/by/emall/yatsevich/BaseTest.java
+++ src/test/java/by/emall/yatsevich/BaseTest.java
@@ -1,4 +1,12 @@
 package by.emall.yatsevich;
 
+import by.emall.yatsevich.ui.driver.ChromeDriverSingleton;
+import org.junit.jupiter.api.AfterEach;
+
 public class BaseTest {
+
+    @AfterEach
+    public void shutUp() {
+        ChromeDriverSingleton.closeDriver();
+    }
 }
